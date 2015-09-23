9654af128af4aaa6a58be6d6fd128945856ac8ca
Tobias Lahmann <tobias.lahmann@uni-ulm.de>
2015-09-23 22:32:54 +0200

    faster, slower, change permutation and object
    very basic

--- GDG-Framework/asset/lib/animation.js
+++ GDG-Framework/asset/lib/animation.js
@@ -8,13 +8,13 @@ Project:		Grundlagen der Gestaltung 1
 var permutationen =
 [
     [ // Permutation 1
-        [1, 1, 1, 1, 1, 1, 1],
-        [1, 1, 1, 1, 1, 1, 1],
-        [1, 1, 1, 1, 1, 1, 1],
-        [1, 1, 1, 1, 1, 1, 1],
-        [1, 1, 1, 1, 1, 1, 1],
-        [1, 1, 1, 1, 1, 1, 1],
-        [1, 1, 1, 1, 1, 1, 1]
+        [1, 1, 5, 1, 5, 1, 1],
+        [1, 5, 1, 5, 1, 5, 1],
+        [5, 1, 5, 1, 5, 1, 5],
+        [1, 5, 1, 5, 1, 5, 1],
+        [5, 1, 5, 1, 5, 1, 5],
+        [1, 5, 1, 5, 1, 5, 1],
+        [1, 1, 5, 1, 5, 1, 1]
     ],
     [ // Permutation 2
         [7, 7, 7, 7, 7, 7, 7],
@@ -28,8 +28,8 @@ var permutationen =
     // hier weitetere Permutationen einfuegen
 ];
 
-var currentPerm = 1; // Speichert die aktuelle Permutation
-var currentObject = 1; // Speichert das akutelle Objekt
+var currentPerm = 0; // Speichert die aktuelle Permutation
+var currentObject = 0; // Speichert das akutelle Objekt
 
 var running = Boolean(true);// spielt ab
 var animationrate = 100; // ms
@@ -43,117 +43,109 @@ var animat = [];
 function initialize() {
     for (i = 0; i < 7; i++) {
         var img = new Image();
-        img.src = "inhalte/anim/group1/" + (i + 1) + ".png";
+        img.src = "inhalte/anim/group" + (currentObject + 1) + "/" + (i + 1) + ".png";
         animat[i] = img;
     }
+    draw();
     //window.requestAnimationFrame(draw);
 }
-
-
 
 var context = document.getElementById('canvas').getContext('2d');
 
 initialize();
-setInterval(draw, animationrate);
+//var myVar = setInterval(draw, animationrate);
+
+var fps = 12;
+var now;
+var then = Date.now();
+var interval = 1000 / fps;
+var delta;
 
 function draw() {
     if (running) {
-        // clear canvas
-        context.clearRect(0, 0, canvas.width, canvas.height);
+        requestAnimationFrame(draw);
 
-        for (i = 0; i < 7; i++) {
-            for (j = 0; j < 7; j++) {
-                var foo = ((permutationen[currentPerm][i][j] += 1) % 7);
-                context.drawImage(animat[foo],
-                                (i * animat[foo].width) + animationPositioX,
-                                (j * animat[foo].height) + animationPositioY,
-                                animat[foo].width,
-                                animat[foo].height);
+        now = Date.now();
+        delta = now - then;
+
+        if (delta > interval) {
+            then = now - (delta % interval);
+
+            // clear canvas
+            context.clearRect(0, 0, canvas.width, canvas.height);
+
+            for (i = 0; i < 7; i++) {
+                for (j = 0; j < 7; j++) {
+                    var foo = ((permutationen[currentPerm][i][j] += 1) % 7);
+                    context.drawImage(animat[foo],
+                                    (i * animat[foo].width) + animationPositioX,
+                                    (j * animat[foo].height) + animationPositioY,
+                                    animat[foo].width,
+                                    animat[foo].height);
+                }
+                //console.log(foo);
             }
-            //console.log(foo);
         }
     }
 };
 
 // funktion: pausieren/stoppen der currentAnimation
 function pauseAnimation() {
-    console.log("pauseAnimation");
+    //console.log("pauseAnimation");
     running = false;
+    draw();
 }
 
 // funktion: (wieder-)abspielen der currentAnimation;
 function playAnimation() {
-    console.log("playAnimation");
+    //console.log("playAnimation");
     running = true;
+    draw();
 }
 
 // funktion: geschwindigkeit erhoehen;
 function faster() {
-    console.log("faster");
-    // Framerate limitiert auf 40 fps maximal
-    if (stage.frameRate < 40) {
-        this.stage.frameRate += 2;
+    //console.log("faster");
+    if (fps < 30) {
+        fps += 2;
+        interval = 1000 / fps;
     }
+    //console.log(fps);
 }
 
 // funktion: geschwindigkeit verringern
 function slower() {
-    console.log("slower");
-    // Framerate limitiert auf 5 fps minimal
-    if (stage.frameRate >= 5) {
-        this.stage.frameRate -= 2;
+    //console.log("slower");
+    if (fps >= 5) {
+        fps -= 2;
+        interval = 1000 / fps;
     }
 }
 
 function changePermutation() {
     console.log("changePermutation");
-    switch (currentPermutationNumber) {
-        case 1:
-            currentPerm = 2;
-            break;
-        case 2:
-            currentPerm = 3;
-            break;
-        case 3:
-            currentPerm = 4;
-            break;
-        case 4:
-            currentPerm = 5;
-            break;
-        case 5:
-            currentPerm = 6;
-            break;
-        case 6:
-            currentPerm = 7;
-            break;
-        default:
-            currentPerm = 1;
-    }
-
-    //initialize();
+    currentPerm += 1;
+    currentPerm %= permutationen.length;
+    initialize();
 }
 
 function changeObject() {
     console.log("changeObject");
-    switch (currentObject) {
-        case 1:
-            currentObject = 2;
-            break;
-        case 2:
-            currentObject = 3;
-            break;
-        case 3:
-            currentObject = 4;
-            break;
-        case 4:
-            currentObject = 5;
-            break;
-        default:
-            currentObject = 1;
-    }
-
-    //initialize();
+    currentObject += 1;
+    currentObject %= permutationen.length;
+    initialize();
 }
 
+function countFolders() {
+    if (window.XMLHttpRequest) {// code for IE7+, Firefox, Chrome, Opera, Safari
+        xmlhttp = new XMLHttpRequest();
+    }
+    else {// code for IE6, IE5
+        xmlhttp = new ActiveXObject("Scripting.FileSystemObject");
+    }
+    //var myFileSysObj = new ActiveXObject("Scripting.FileSystemObject");
+    var myFolder = xmlhttp.GetFolder("./inhalte/anim/");
+    var myFolderCollection = myFolder.SubFolders;
+};
 
-
+//console.log(countFolders());
