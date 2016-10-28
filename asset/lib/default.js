867756e2c3514f672a228a146c7e07a2104b9712
Tobias Lahmann <tobias.lahmann@uni-ulm.de>
2016-10-28 23:10:02 +0200

    Refactor fill navigation function

--- asset/lib/default.js
+++ asset/lib/default.js
@@ -1,4 +1,4 @@
-﻿// JavaScript document
+// JavaScript document
 /* 
     Grundlagen der Gestaltung Framework
     Univerität Ulm
@@ -359,34 +359,40 @@ function pictureInput() {
 // helper to fill the navigation with info
 // string concat
 function fillNavigation() {
-    var string = '<ul>';
+    var outerUl = document.createElement("ul");
     var c = 0;
 
     for (var i = 0; i < json.DOKU.ABSCHNITT.length; i++) {
-        string += '<li><details><summary>';
-        string += json.DOKU.ABSCHNITT[i]['@attributes']['titel']
-        string += '</summary><ul>';
+        var outerLi = document.createElement("li");
+        var det = document.createElement("details");
+        var sum = document.createElement("summary");
+        sum.innerHTML = json.DOKU.ABSCHNITT[i]['@attributes']['titel']
+        var innerUl = document.createElement("ul");
+        var innerLi;
+        
         if (json.DOKU.ABSCHNITT[i].INHALT.length > 0) {
             for (var j = 0; j < json.DOKU.ABSCHNITT[i].INHALT.length; j++) {
-                string += '<li data-link=\"';
-                string += c + ':' + i + ',' + j + '\"';
-                string += 'onclick=\"jumpTo(this)\">';
-                string += json.DOKU.ABSCHNITT[i].INHALT[j]['@attributes']['titel'];
-                string += '</li>';
+                innerLi = document.createElement("li");
+                innerLi.setAttribute('data-link', c + ':' + i + ',' + j + '\"');
+                innerLi.setAttribute('onclick', 'jumpTo(this)');
+                innerLi.innerHTML = json.DOKU.ABSCHNITT[i].INHALT[j]['@attributes']['titel'];
+                innerUl.appendChild(innerLi);
                 c++;
             }
         } else {
-            string += '<li data-link=\"';
-            string += c + ':' + i + ',' + j + '\"';
-            string += 'onclick=\"jumpTo(this)\">';
-            string += json.DOKU.ABSCHNITT[i].INHALT['@attributes']['titel'];
-            string += '</li>';
+            innerLi = document.createElement("li");
+            innerLi.setAttribute('data-link', c + ':' + i + ',' + j + '\"');
+            innerLi.setAttribute('onclick', 'jumpTo(this)');
+            innerLi.innerHTML = json.DOKU.ABSCHNITT[i].INHALT['@attributes']['titel'];
+            innerUl.appendChild(innerLi);
             c++;
         }
-        string += '</ul></details>';
+        det.appendChild(sum);
+        det.appendChild(innerUl);
+        outerLi.appendChild(det);
+        outerUl.appendChild(outerLi);
     }
-    string += '</ul>';
-    document.getElementById("navscroll").innerHTML = string;
+    document.getElementById("navscroll").appendChild(outerUl);
     return;
 };
 
@@ -408,10 +414,10 @@ $("input[name='navTrigger']").change(function () {
 function jumpTo(id) {
     var data = (id.dataset.link).split(":");
     var jump = data[1].split(",");
-    section = jump[0];
-    content = jump[1];
+    section = parseInt(jump[0]);
+    content = parseInt(jump[1]);
 
-    sectionsPointer = data[0];
+    sectionsPointer = parseInt(data[0]);
 
     redraw();
 };
@@ -435,367 +441,3 @@ function triggerBorder() {
         $('#flashCanvas').css("border-color", "#FFF");
     }
 };
-
-/* SIMPLEBAR */
-; (function (factory) {
-    'use strict';
-    if (typeof define === 'function' && define.amd) {
-        // Register as an anonymous AMD module:
-        define(['jquery'], factory($, window, document, undefined));
-    } else if (typeof exports === 'object') {
-        // Node/CommonJS:
-        factory(require('jquery'), window, document, undefined);
-    } else {
-        // Browser globals:
-        factory(window.jQuery, window, document, undefined);
-    }
-}(function ($, window, document, undefined) {
-
-
-    /**
-     * Calculate scrollbar width
-     *
-     * Called only once as a constant variable: we assume that scrollbar width never change
-     * 
-     * Original function by Jonathan Sharp:
-     * http://jdsharp.us/jQuery/minute/calculate-scrollbar-width.php
-     */
-    var SCROLLBAR_WIDTH = scrollbarWidth();
-
-    function scrollbarWidth() {
-        // Append a temporary scrolling element to the DOM, then measure
-        // the difference between its outer and inner elements.
-        var tempEl = $('<div class="scrollbar-width-tester" style="width:50px;height:50px;overflow-y:scroll;top:-200px;left:-200px;"><div style="height:100px;"></div>'),
-            width = 0,
-            widthMinusScrollbars = 0;
-
-        $('body').append(tempEl);
-
-        width = $(tempEl).innerWidth(),
-        widthMinusScrollbars = $('div', tempEl).innerWidth();
-
-        tempEl.remove();
-
-        return (width - widthMinusScrollbars);
-    }
-
-
-
-    // SimpleBar Constructor
-    function SimpleBar(element, options) {
-        this.el = element,
-        this.$el = $(element),
-        this.$track,
-        this.$scrollbar,
-        this.dragOffset,
-        this.flashTimeout,
-        this.$contentEl = this.$el,
-        this.$scrollContentEl = this.$el,
-        this.scrollDirection = 'vert',
-        this.scrollOffsetAttr = 'scrollTop',
-        this.sizeAttr = 'height',
-        this.scrollSizeAttr = 'scrollHeight',
-        this.offsetAttr = 'top';
-
-        this.options = $.extend({}, SimpleBar.DEFAULTS, options);
-
-        this.init();
-    }
-
-    SimpleBar.DEFAULTS = {
-        wrapContent: true,
-        autoHide: true
-    };
-
-    SimpleBar.prototype.init = function () {
-        // If scrollbar is a floating scrollbar, disable the plugin
-        if (SCROLLBAR_WIDTH === 0) {
-            this.$el.css('overflow', 'auto');
-
-            return;
-        }
-
-        if (this.$el.data('simplebar-direction') === 'horizontal' || this.$el.hasClass('simplebar horizontal')) {
-            this.scrollDirection = 'horiz';
-            this.scrollOffsetAttr = 'scrollLeft';
-            this.sizeAttr = 'width';
-            this.scrollSizeAttr = 'scrollWidth';
-            this.offsetAttr = 'left';
-        }
-
-        if (this.options.wrapContent) {
-            this.$el.wrapInner('<div class="simplebar-scroll-content"><div class="simplebar-content"></div></div>');
-        }
-
-        this.$contentEl = this.$el.find('.simplebar-content');
-
-        this.$el.prepend('<div class="simplebar-track"><div class="simplebar-scrollbar"></div></div>');
-        this.$track = this.$el.find('.simplebar-track');
-        this.$scrollbar = this.$el.find('.simplebar-scrollbar');
-
-        this.$scrollContentEl = this.$el.find('.simplebar-scroll-content');
-
-        this.resizeScrollContent();
-
-        if (this.options.autoHide) {
-            this.$el.on('mouseenter', $.proxy(this.flashScrollbar, this));
-        }
-
-        this.$scrollbar.on('mousedown', $.proxy(this.startDrag, this));
-        this.$scrollContentEl.on('scroll', $.proxy(this.startScroll, this));
-
-        this.resizeScrollbar();
-
-        if (!this.options.autoHide) {
-            this.showScrollbar();
-        }
-    };
-
-
-    /**
-     * Start scrollbar handle drag
-     */
-    SimpleBar.prototype.startDrag = function (e) {
-        // Preventing the event's default action stops text being
-        // selectable during the drag.
-        e.preventDefault();
-
-        // Measure how far the user's mouse is from the top of the scrollbar drag handle.
-        var eventOffset = e.pageY;
-        if (this.scrollDirection === 'horiz') {
-            eventOffset = e.pageX;
-        }
-        this.dragOffset = eventOffset - this.$scrollbar.offset()[this.offsetAttr];
-
-        $(document).on('mousemove', $.proxy(this.drag, this));
-        $(document).on('mouseup', $.proxy(this.endDrag, this));
-    };
-
-
-    /**
-     * Drag scrollbar handle
-     */
-    SimpleBar.prototype.drag = function (e) {
-        e.preventDefault();
-
-        // Calculate how far the user's mouse is from the top/left of the scrollbar (minus the dragOffset).
-        var eventOffset = e.pageY,
-            dragPos = null,
-            dragPerc = null,
-            scrollPos = null;
-
-        if (this.scrollDirection === 'horiz') {
-            eventOffset = e.pageX;
-        }
-
-        dragPos = eventOffset - this.$track.offset()[this.offsetAttr] - this.dragOffset;
-        // Convert the mouse position into a percentage of the scrollbar height/width.
-        dragPerc = dragPos / this.$track[this.sizeAttr]();
-        // Scroll the content by the same percentage.
-        scrollPos = dragPerc * this.$contentEl[0][this.scrollSizeAttr];
-
-        this.$scrollContentEl[this.scrollOffsetAttr](scrollPos);
-    };
-
-
-    /**
-     * End scroll handle drag
-     */
-    SimpleBar.prototype.endDrag = function () {
-        $(document).off('mousemove', this.drag);
-        $(document).off('mouseup', this.endDrag);
-    };
-
-
-    /**
-     * Resize scrollbar
-     */
-    SimpleBar.prototype.resizeScrollbar = function () {
-        if (SCROLLBAR_WIDTH === 0) {
-            return;
-        }
-
-        var contentSize = this.$contentEl[0][this.scrollSizeAttr],
-            scrollOffset = this.$scrollContentEl[this.scrollOffsetAttr](), // Either scrollTop() or scrollLeft().
-            scrollbarSize = this.$track[this.sizeAttr](),
-            scrollbarRatio = scrollbarSize / contentSize,
-            // Calculate new height/position of drag handle.
-            // Offset of 2px allows for a small top/bottom or left/right margin around handle.
-            handleOffset = Math.round(scrollbarRatio * scrollOffset) + 2,
-            handleSize = Math.floor(scrollbarRatio * (scrollbarSize - 2)) - 2;
-
-
-        if (scrollbarSize < contentSize) {
-            if (this.scrollDirection === 'vert') {
-                this.$scrollbar.css({ 'top': handleOffset, 'height': handleSize });
-            } else {
-                this.$scrollbar.css({ 'left': handleOffset, 'width': handleSize });
-            }
-            this.$track.show();
-        } else {
-            this.$track.hide();
-        }
-    };
-
-
-    /**
-     * On scroll event handling
-     */
-    SimpleBar.prototype.startScroll = function (e) {
-        // Simulate event bubbling to root element
-        this.$el.trigger(e);
-
-        this.flashScrollbar();
-    };
-
-
-    /**
-     * Flash scrollbar visibility
-     */
-    SimpleBar.prototype.flashScrollbar = function () {
-        this.resizeScrollbar();
-        this.showScrollbar();
-    };
-
-
-    /**
-     * Show scrollbar
-     */
-    SimpleBar.prototype.showScrollbar = function () {
-        this.$scrollbar.addClass('visible');
-
-        if (!this.options.autoHide) {
-            return;
-        }
-        if (typeof this.flashTimeout === 'number') {
-            window.clearTimeout(this.flashTimeout);
-        }
-
-        this.flashTimeout = window.setTimeout($.proxy(this.hideScrollbar, this), 1000);
-    };
-
-
-    /**
-     * Hide Scrollbar
-     */
-    SimpleBar.prototype.hideScrollbar = function () {
-        this.$scrollbar.removeClass('visible');
-        if (typeof this.flashTimeout === 'number') {
-            window.clearTimeout(this.flashTimeout);
-        }
-    };
-
-
-    /**
-     * Resize content element
-     */
-    SimpleBar.prototype.resizeScrollContent = function () {
-        var is_firefox = /firefox/i.test(navigator.userAgent);
-        if (!is_firefox) {
-            return;
-        }
-        if (this.scrollDirection === 'vert') {
-            this.$scrollContentEl.width(this.$el.width() + SCROLLBAR_WIDTH);
-            this.$scrollContentEl.height(this.$el.height());
-        } else {
-            this.$scrollContentEl.width(this.$el.width());
-            this.$scrollContentEl.height(this.$el.height() + SCROLLBAR_WIDTH);
-        }
-    };
-
-
-    /**
-     * Recalculate scrollbar
-     */
-    SimpleBar.prototype.recalculate = function () {
-        this.resizeScrollContent();
-        this.resizeScrollbar();
-    };
-
-
-    /**
-     * Getter for original scrolling element
-     */
-    SimpleBar.prototype.getScrollElement = function () {
-        return this.$scrollContentEl;
-    };
-
-
-    /**
-     * Getter for content element
-     */
-    SimpleBar.prototype.getContentElement = function () {
-        return this.$contentEl;
-    };
-
-
-    /**
-     * Data API
-     */
-    $(window).on('load', function () {
-        $('[data-simplebar-direction]').each(function () {
-            $(this).simplebar();
-        });
-    });
-
-
-    /**
-     * Plugin definition
-     */
-    var old = $.fn.simplebar;
-
-    $.fn.simplebar = function (options) {
-        var args = arguments,
-            returns;
-
-        // If the first parameter is an object (options), or was omitted,
-        // instantiate a new instance of the plugin.
-        if (typeof options === 'undefined' || typeof options === 'object') {
-            return this.each(function () {
-
-                // Only allow the plugin to be instantiated once,
-                // so we check that the element has no plugin instantiation yet
-                if (!$.data(this, 'simplebar')) { $.data(this, 'simplebar', new SimpleBar(this, options)); }
-            });
-
-            // If the first parameter is a string
-            // treat this as a call to a public method.
-        } else if (typeof options === 'string') {
-            this.each(function () {
-                var instance = $.data(this, 'simplebar');
-
-                // Tests that there's already a plugin-instance
-                // and checks that the requested public method exists
-                if (instance instanceof SimpleBar && typeof instance[options] === 'function') {
-
-                    // Call the method of our plugin instance,
-                    // and pass it the supplied arguments.
-                    returns = instance[options].apply(instance, Array.prototype.slice.call(args, 1));
-                }
-
-                // Allow instances to be destroyed via the 'destroy' method
-                if (options === 'destroy') {
-                    $.data(this, 'simplebar', null);
-                }
-            });
-
-            // If the earlier cached method
-            // gives a value back return the value,
-            // otherwise return this to preserve chainability.
-            return returns !== undefined ? returns : this;
-        }
-    };
-
-    $.fn.simplebar.Constructor = SimpleBar;
-
-
-    /**
-     * No conflict
-     */
-    $.fn.simplebar.noConflict = function () {
-        $.fn.simplebar = old;
-        return this;
-    };
-
-}));
-
